c67703e2598c9d85e370cd702ac89c9ca7163f6b
thinhph <thinh.ph@ssv.com.vn>
2021-12-29 23:26:38 +0700

    update constant in JsonServiceImpl

--- src/main/java/com/thinhph/JsonToJavaFormat/service/impl/JSONServiceImpl.java
+++ src/main/java/com/thinhph/JsonToJavaFormat/service/impl/JSONServiceImpl.java
@@ -9,10 +9,14 @@ import java.util.LinkedHashMap;
 @Service
 public class JSONServiceImpl implements JSONService {
 
+    public static final String SETTER_FORMAT = "myObject.set";
+    public static final String BUILDER_FORMAT = "MyObject.builder().";
+    public static final String CONSTRUCTOR_FORMAT = "myObject(";
+
     @Override
     public String getJavaConstructorFormat(LinkedHashMap<String, String> jsonMap) {
         StringBuilder constructor = new StringBuilder();
-        constructor.append("Constructor format: \nmyObject(");
+        constructor.append("Constructor format: \n").append(CONSTRUCTOR_FORMAT);
         jsonMap.keySet().forEach(key -> {
 
             if (jsonMap.get(key) == null)
@@ -30,7 +34,7 @@ public class JSONServiceImpl implements JSONService {
     @Override
     public String getJavaBuilderFormat(LinkedHashMap<String, String> jsonMap) {
         StringBuilder builder = new StringBuilder();
-        builder.append("Builder format:\n MyObject.builder().");
+        builder.append("Builder format:\n").append(BUILDER_FORMAT);
         jsonMap.keySet().forEach(key -> {
             if (jsonMap.get(key) == null)
                 builder.append("\n.").append(key).append("(null)");
@@ -50,17 +54,14 @@ public class JSONServiceImpl implements JSONService {
         setter.append("Setter format: ");
         jsonMap.keySet().forEach(key -> {
             if(jsonMap.get(key) == null){
-                setter.append("\n").append("myObject.set")
-                        .append(getKey(key))
+                setter.append("\n").append(SETTER_FORMAT).append(getKey(key))
                         .append("(null);");
             }else if(jsonMap.get(key).equals("")){
-                setter.append("\n").append("myObject.set")
-                        .append(getKey(key))
-                        .append("\"\"");
+                setter.append("\n").append(SETTER_FORMAT).append(getKey(key)).append("\"\"");
             }else if(StringUtils.isNumeric(jsonMap.get(key)))
-                setter.append("\n").append("myObject.set").append(getKey(key)).append("(").append(jsonMap.get(key)).append(");");
+                setter.append("\n").append(SETTER_FORMAT).append(getKey(key)).append("(").append(jsonMap.get(key)).append(");");
             else
-                setter.append("\n").append("myObject.set").append(getKey(key)).append("(\"").append(jsonMap.get(key)).append("\");");
+                setter.append("\n").append(SETTER_FORMAT).append(getKey(key)).append("(\"").append(jsonMap.get(key)).append("\");");
         });
         return setter.toString();
     }
@@ -69,10 +70,8 @@ public class JSONServiceImpl implements JSONService {
         StringBuilder result = new StringBuilder();
         result.append(key.substring(0, 1).toUpperCase());
         for (int i = 1; i < key.length(); i++) {
-            if(key.charAt(i-1) == '_')
-                result.append(key.substring(i, i+1).toUpperCase());
-            else if(key.charAt(i) != '_')
-                result.append(key.charAt(i));
+            if(key.charAt(i-1) == '_') result.append(key.substring(i, i+1).toUpperCase());
+            else if(key.charAt(i) != '_') result.append(key.charAt(i));
         }
         return result.toString();
     }
