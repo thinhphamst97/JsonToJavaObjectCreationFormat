20a95db0068166ab72130aa2a0d6391036b0b760
thinhph <thinh.ph@ssv.com.vn>
2022-01-11 14:19:23 +0700

    update controller

--- src/main/java/com/thinhph/JsonToJavaFormat/controller/JsonToJavaController.java
+++ src/main/java/com/thinhph/JsonToJavaFormat/controller/JsonToJavaController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.LinkedHashMap;
 
 @RestController
-@RequestMapping
 public class JsonToJavaController {
 
     @Autowired
